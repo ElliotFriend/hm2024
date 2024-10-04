e7484732ffe5cc00dd61c76766e62cf0b340cd89
Elliot Voris <elliot@stellar.org>
2024-10-04 13:49:16 -0500

    updates and formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	import '../app.postcss';
+    import '../app.postcss';
 </script>
 
-<slot />
+<div class="container h-full mx-auto flex justify-center items-center">
+    <div class="space-y-5">
+        <slot />
+    </div>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+    <title>About</title>
+    <meta name="description" content="About this app" />
+</svelte:head>
+
+<h1 class="h1">About this app</h1>
+
+<p>
+    This is a <a class="anchor" href="https://kit.svelte.dev" target="_blank">SvelteKit</a> app
+    (created via <a class="anchor" href="https://skeleton.dev" target="_blank">Skeleton</a>). You
+    can make your own by typing the following into your command line and following the prompts:
+</p>
+
+<pre>npm create skeleton-app@latest</pre>
+
+<p>Or, for a more "vanilla" SvelteKit experience:</p>
+
+<pre>npm create svelte@latest</pre>
+
+<p>
+    The page you're looking at is purely static HTML, with no client-side interactivity needed.
+    Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+    the devtools network panel and reloading.
+</p>
