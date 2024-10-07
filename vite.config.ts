0c5980336f424bae918253075b7ea2711d181f81
Elliot Voris <elliot@stellar.org>
2024-10-07 12:14:17 -0500

    allow sveltekit to access packages in `./packages`

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        fs: {
+            allow: ['./packages']
+        }
+    },
     plugins: [sveltekit(), purgeCss()],
 });
