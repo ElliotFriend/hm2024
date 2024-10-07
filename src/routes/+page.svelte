15511edbc6d116f6322ce4988e586a82f874b740
Elliot Voris <elliot@stellar.org>
2024-10-07 12:23:50 -0500

    add placeholder functions for helloworld bindings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
-<h1 class="h1">Let's get cracking bones!</h1>
+<script lang="ts">
+    // This may fail, if you removed or renamed your `hello_world` contract in
+    // the `/contracts` directory.
+    // import helloWorld from "$lib/contracts/hello_world";
+    // let hello = helloWorld.hello({
+    //     to: 'Meridian',
+    // })
+</script>
+
+<!-- {#await hello then { result } }
+    <h1 class="h1">{result.join(' ')}!</h1>
+{/await} -->
+<h2 class="h2">Let's get cracking bones!</h2>
 <p>Start by exploring:</p>
 <ul>
     <li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
