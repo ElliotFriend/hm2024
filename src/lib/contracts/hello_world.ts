ba1d48fb048dda502c34c200b17a17bb0911b8e3
Elliot Voris <elliot@stellar.org>
2024-10-10 17:31:32 -0500

    more formatting finds

--- src/lib/contracts/hello_world.ts
+++ src/lib/contracts/hello_world.ts
@@ -1,8 +1,5 @@
 import * as Client from 'hello_world';
-import {
-    PUBLIC_STELLAR_RPC_URL,
-    PUBLIC_STELLAR_NETWORK_PASSPHRASE,
-} from '$env/static/public';
+import { PUBLIC_STELLAR_RPC_URL, PUBLIC_STELLAR_NETWORK_PASSPHRASE } from '$env/static/public';
 
 export default new Client.Client({
     contractId: Client.networks.testnet.contractId,
